fb1bbc93f8d03d36239ab0a2d5ebf8468b4bada0
Egor Boldyrev <boldyreved@gmail.com>
2019-02-05 19:48:39 +0700

    adding lab1 template to repo

---
+++ lab1_calendar/lab1_calendar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab1_calendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
