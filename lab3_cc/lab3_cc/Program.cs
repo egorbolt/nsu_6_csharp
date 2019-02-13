adbfb4345222da729fdf47eb58621fbfef067c81
Egor Boldyrev <boldyreved@gmail.com>
2019-02-13 17:30:43 +0700

    adding lab3

---
+++ lab3_cc/lab3_cc/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace lab3_cc {
+    class Program {
+        static int _linesSum = 0;
+        
+        static void Main(string[] args) {
+            CountLines(".");
+            Console.WriteLine(_linesSum);
+        }
+
+        static void CountLines(string path) {
+            foreach (string directory in Directory.GetDirectories(path)) {
+                CountLines(directory);
+            }
+
+            foreach (string file in Directory.GetFiles(path)) {
+                if (Path.GetExtension(file).Equals(".cs")) {
+                    using (StreamReader sr = new StreamReader(file, System.Text.Encoding.Default)) {
+                        string line;
+                        while ((line = sr.ReadLine()) != null) {
+                            if (line.Length >= 2) {
+                                string sub = line.Substring(0, 2);
+                                if (!sub.Equals("//")) {
+                                    _linesSum++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
