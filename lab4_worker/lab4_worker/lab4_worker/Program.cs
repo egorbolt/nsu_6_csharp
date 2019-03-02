f41232cb2634820d60b4690d020e7ed7eba788fd
Egor Boldyrev <boldyreved@gmail.com>
2019-03-02 21:59:30 +0700

    sharing with windows pc

---
+++ lab4_worker/lab4_worker/lab4_worker/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lab4_worker {
+    class Program {
+        static void Main(string[] args) {
+            using (ApplicationContext db = new ApplicationContext()) {
+                Worker worker1 = new Worker {FIO = "Boldyrev Egor Dmitrievich"};
+
+                db.Workers.Add(worker1);
+                db.SaveChanges();
+                Console.WriteLine("saved");
+            }
+        }
+    }
+
+}
---
+++ lab4_worker/lab4_worker/lab4_worker/Worker.cs
@@ -0,0 +1,6 @@
+namespace lab4_worker {
+    public class Worker {
+        public int Id { get; set; } 
+        public string FIO { get; set; }
+    }
+}
